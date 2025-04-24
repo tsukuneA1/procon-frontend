c2383ec44db47d6d7601ac710098c7b3c48cee42
tsukuneA1 <tkschr1125@gmail.com>
2025-04-24 15:42:52 +0900

    test

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -3,6 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
 import { MessageCircle, Heart, Repeat2 } from "lucide-react"
 import Link from "next/link"
+import Link from "next/link"
 
 export const PostCard = () => {
   return (
