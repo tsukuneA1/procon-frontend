fb375b226aa06a3047fba418d7ed752dcb9fcc33
tsukuneA1 <tkschr1125@gmail.com>
2025-04-24 11:42:21 +0900

    仮コンポーネントと仮リプライ機能のみクライアント側でデモを作成。vercelみてね

---
+++ src/components/layout/main/header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+    return (
+        <h1>header</h1>
+    )
+}
---
+++ src/layouts/main/layout.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/layout/main/header";
+
+export const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='flex min-h-screen flex-col'>
+      <Header />
+      <main className='mx-auto flex w-full max-w-3xl flex-grow flex-col items-center p-8'>
+        {children}
+      </main>
+    </div>
+  );
+};
