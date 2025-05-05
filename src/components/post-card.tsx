fdbe49cc40dd7a9d02516b060d43abe182d50bc3
doguto <doguto110@gmail.com>
2025-05-06 02:14:49 +0900

    Feat: 三点リーダーと共有ボタンの追加

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -2,7 +2,13 @@ import { Card, CardContent, CardHeader } from './ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { getTimeDistance } from '../lib/utils';
-import { MessageCircle, Heart, Repeat2 } from 'lucide-react';
+import {
+  MessageCircle,
+  Heart,
+  Repeat2,
+  MoreHorizontal,
+  Share,
+} from 'lucide-react';
 import Link from 'next/link';
 import type { Post } from '@/types/post';
 
@@ -24,6 +30,12 @@ export const PostCard = ({ post }: { post: Post }) => {
             <span className='text-gray-400 text-xs'>
               {getTimeDistance(post.created_at)}
             </span>
+            <Link
+              href={`/postinfo/${post.id}`}
+              className='ml-auto text-gray-500'
+            >
+              <MoreHorizontal className='h-4 w-4' />
+            </Link>
           </CardHeader>
           <Link href={`/post/${post.id}`}>
             <CardContent className='p-0 text-base text-zinc-800 dark:text-zinc-200'>
@@ -52,6 +64,13 @@ export const PostCard = ({ post }: { post: Post }) => {
               className='flex items-center gap-1 px-2'
             >
               <Repeat2 className='h-4 w-4' />1
+            </Button>
+            <Button
+              variant='ghost'
+              size='sm'
+              className='flex items-center gap-1 px-2'
+            >
+              <Share />
             </Button>
           </div>
         </div>
