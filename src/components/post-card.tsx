fc7b4ca09da8d9676e656c6619e347d0ed80d397
doguto <doguto110@gmail.com>
2025-05-07 16:45:57 +0900

    Feat: Drawerの仮実装&Postの情報の種類を追加

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,12 +1,13 @@
 import { Card, CardContent, CardHeader } from './ui/card';
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
+import { Drawer, DrawerContent, DrawerTitle, DrawerTrigger } from './ui/drawer';
 import { getTimeDistance } from '../lib/utils';
+import { UserIcon } from './user-icon';
 import {
-  MessageCircle,
   Heart,
-  Repeat2,
+  MessageCircle,
   MoreHorizontal,
+  Repeat2,
   Share,
 } from 'lucide-react';
 import Link from 'next/link';
@@ -16,30 +17,32 @@ export const PostCard = ({ post }: { post: Post }) => {
   return (
     <Card className='w-2xl rounded-none p-4 pb-2'>
       <div className='flex gap-4'>
-        <Link href={`/users/${post.user_id}`}>
-          <Avatar className='top-2px border-1 border-gray-300'>
-            <AvatarImage src={post.user.image} />
-            <AvatarFallback>U</AvatarFallback>
-          </Avatar>
-        </Link>
+        <UserIcon post={post} />
         <div className='flex flex-1 flex-col'>
           <CardHeader className='flex items-end p-0 pb-0.5'>
-            <Link href={`/users/${post.user_id}`}>
-              <span className='font-semibold text-sm'>{post.user.name}</span>
+            <Link href={`/users/${post.post.user_id}`}>
+              <span className='font-semibold text-sm'>
+                {post.post.user.name}
+              </span>
             </Link>
             <span className='text-gray-400 text-xs'>
-              {getTimeDistance(post.created_at)}
+              {getTimeDistance(post.post.created_at)}
             </span>
-            <Link
-              href={`/postinfo/${post.id}`}
-              className='ml-auto text-gray-500'
-            >
-              <MoreHorizontal className='h-4 w-4' />
-            </Link>
+            <Drawer>
+              <DrawerTrigger className='ml-auto text-gray-500'>
+                <Button variant='ghost' size='icon'>
+                  <MoreHorizontal className='h-4 w-4' />
+                </Button>
+              </DrawerTrigger>
+              <DrawerContent>
+                <DrawerTitle />
+                hoge
+              </DrawerContent>
+            </Drawer>
           </CardHeader>
           <Link href={`/post/${post.id}`}>
             <CardContent className='p-0 text-base text-zinc-800 dark:text-zinc-200'>
-              {post.content}
+              {post.post.content}
             </CardContent>
           </Link>
           <div className='mt-3 flex gap-4 text-sm text-zinc-500'>
@@ -49,21 +52,23 @@ export const PostCard = ({ post }: { post: Post }) => {
               className='flex items-center gap-1 px-2'
             >
               <Heart className='h-4 w-4' />
-              12
+              {post.like_num}
             </Button>
             <Button
               variant='ghost'
               size='sm'
               className='flex items-center gap-1 px-2'
             >
-              <MessageCircle className='h-4 w-4' />3
+              <MessageCircle className='h-4 w-4' />
+              {post.reply_num}
             </Button>
             <Button
               variant='ghost'
               size='sm'
               className='flex items-center gap-1 px-2'
             >
-              <Repeat2 className='h-4 w-4' />1
+              <Repeat2 className='h-4 w-4' />
+              {post.repost_num}
             </Button>
             <Button
               variant='ghost'
