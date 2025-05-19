452f3e57d4f1a11df5c7a1ccadabb0a0dbfb44ba
doguto <doguto110@gmail.com>
2025-05-19 13:58:59 +0900

    Fix: PostCard書き換えてしまっていたので復旧

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,88 +1,160 @@
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
-import { Button } from './ui/button';
-import Link from 'next/link';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover';
-import { useState } from 'react';
-import { UserPlus, SquareArrowOutUpRight, UserMinus } from 'lucide-react';
+"use client";
 
-type UserIconInfo = {
-  id: number;
-  image: string;
-  is_following: boolean;
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import type { Post } from "@/types/post";
+import { useMediaQuery } from "@mui/material";
+import {
+	Bookmark,
+	EyeOff,
+	Heart,
+	Link2,
+	MessageCircle,
+	MessageSquareOff,
+	MoreHorizontal,
+	Repeat2,
+	Share,
+	ShieldAlert,
+	UserRoundX,
+} from "lucide-react";
+import Link from "next/link";
+import { getTimeDistance } from "../lib/utils";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardHeader } from "./ui/card";
+import { Drawer, DrawerContent, DrawerTitle, DrawerTrigger } from "./ui/drawer";
+import { UserIcon } from "./user-icon";
+
+export const PostCard = ({ post }: { post: Post }) => {
+	const isDesktop = useMediaQuery("(min-width: 768px)");
+
+	return (
+		<Card className="w-full rounded-none p-4 pb-2 sm:w-2xs md:w-2xl">
+			<div className="flex gap-4">
+				<UserIcon iconInfo={{ ...post.user }} />
+				<div className="flex flex-1 flex-col">
+					<CardHeader className="flex items-center p-0 pb-0.5">
+						<Link href={`/users/${post.user_id}`}>
+							<span className="font-semibold text-sm">{post.user.name}</span>
+						</Link>
+						<span className="text-gray-400 text-xs">
+							{getTimeDistance(post.created_at)}
+						</span>
+						{isDesktop ? (
+							<Popover>
+								<PopoverTrigger asChild className="ml-auto text-gray-500">
+									<Button variant="ghost" size="icon">
+										<MoreHorizontal className="h-4 w-4" />
+									</Button>
+								</PopoverTrigger>
+								<PopoverContent
+									align="end"
+									className="m-0 flex flex-col divide-y p-0"
+								>
+									<PostOptions />
+								</PopoverContent>
+							</Popover>
+						) : (
+							<Drawer>
+								<DrawerTrigger asChild className="ml-auto text-gray-500">
+									<Button variant="ghost" size="icon">
+										<MoreHorizontal className="h-4 w-4" />
+									</Button>
+								</DrawerTrigger>
+								<DrawerContent className="flex flex-col bg-neutral-200 p-4">
+									<DrawerTitle className="mt-3" />
+									<PostOptions />
+								</DrawerContent>
+							</Drawer>
+						)}
+					</CardHeader>
+					<Link href={`/post/${post.id}`}>
+						<CardContent className="p-0 text-base text-zinc-800 dark:text-zinc-200">
+							{post.content}
+						</CardContent>
+					</Link>
+					<div className="mt-3 flex gap-4 text-sm text-zinc-500">
+						<Button
+							variant="ghost"
+							size="sm"
+							className="flex items-center gap-1 px-2"
+						>
+							<Heart className="h-4 w-4" />
+							{post.likes_count}
+						</Button>
+						<Button
+							variant="ghost"
+							size="sm"
+							className="flex items-center gap-1 px-2"
+						>
+							<MessageCircle className="h-4 w-4" />
+							{post.replies_count}
+						</Button>
+						<Button
+							variant="ghost"
+							size="sm"
+							className="flex items-center gap-1 px-2"
+						>
+							<Repeat2 className="h-4 w-4" />
+							{post.reposts_count}
+						</Button>
+						<Button
+							variant="ghost"
+							size="sm"
+							className="flex items-center gap-1 px-2"
+						>
+							<Share />
+						</Button>
+					</div>
+				</div>
+			</div>
+		</Card>
+	);
 };
 
-export const UserIcon = ({ iconInfo }: { iconInfo: UserIconInfo }) => {
-  const [isFollowing, setIsFollowing] = useState(iconInfo.is_following);
-  const handleFollow = async () => {
-    const res = await fetch('/api/follow', {
-      method: 'POST',
-      body: JSON.stringify({ user_id: iconInfo.id }),
-    });
-    setIsFollowing(res.ok);
-    if (res.ok) return;
-
-    alert('フォローに失敗しました');
-  };
-
-  if (iconInfo.is_following) {
-    return (
-      <Link href={`/users/${iconInfo.id}`}>
-        <Avatar className='top-2 border-1 border-gray-300'>
-          <AvatarImage src={iconInfo.image} />
-          <AvatarFallback>U</AvatarFallback>
-        </Avatar>
-      </Link>
-    );
-  }
-
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button variant='ghost' size='icon' className='top-2 rounded-full'>
-          <Avatar className='border border-gray-300'>
-            <AvatarImage src={iconInfo.image} />
-            <AvatarFallback>U</AvatarFallback>
-          </Avatar>
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent align='start' className='m-0 flex flex-col divide-y p-0'>
-        {isFollowing ? (
-          <Button
-            variant='ghost'
-            size='sm'
-            className='flex justify-between'
-            onClick={handleFollow}
-          >
-            <span>フォローをやめる</span>
-            <UserMinus />
-          </Button>
-        ) : (
-          <Button
-            variant='ghost'
-            size='sm'
-            className='flex justify-between'
-            onClick={handleFollow}
-          >
-            <span>フォロー</span>
-            <UserPlus />
-          </Button>
-        )}
-        <Button
-          asChild
-          variant='ghost'
-          size='sm'
-          className='flex justify-between'
-        >
-          <Link href={`/users/${iconInfo.id}`}>
-            <span>プロフィールページ</span>
-            <SquareArrowOutUpRight />
-          </Link>
-        </Button>
-      </PopoverContent>
-    </Popover>
-  );
+const PostOptions = () => {
+	return (
+		<div>
+			<div className="m-1 flex flex-col divide-y rounded-md bg-white">
+				<Button variant="ghost" size="sm" className="flex justify-between">
+					<span>保存</span>
+					<Bookmark />
+				</Button>
+				<Button variant="ghost" size="sm" className="flex justify-between">
+					<span>興味なし</span>
+					<EyeOff />
+				</Button>
+			</div>
+			<div className="m-1 flex flex-col divide-y rounded-md bg-white">
+				<Button variant="ghost" size="sm" className="flex justify-between">
+					<span>リンクをコピー</span>
+					<Link2 />
+				</Button>
+			</div>
+			<div className="m-1 flex flex-col divide-y rounded-md bg-white">
+				<Button variant="ghost" size="sm" className="flex justify-between">
+					<span>ミュート</span>
+					<MessageSquareOff />
+				</Button>
+				<Button
+					variant="ghost"
+					size="sm"
+					className="flex justify-between text-red-500"
+				>
+					<span>ブロック</span>
+					<UserRoundX />
+				</Button>
+				<Button
+					variant="ghost"
+					size="sm"
+					className="flex justify-between text-red-500"
+				>
+					<span>報告する</span>
+					<ShieldAlert />
+				</Button>
+			</div>
+		</div>
+	);
 };
