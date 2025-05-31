7863cee863e8ef006322d1a4d4cf443cf575772b
doguto <doguto110@gmail.com>
2025-05-31 22:40:22 +0900

    Perf: Footerにボタンを追加

--- src/components/layout/main/footer.tsx
+++ src/components/layout/main/footer.tsx
@@ -1,36 +1,53 @@
-import { House } from "lucide-react";
+import { Heart, House, Search, User } from "lucide-react";
+import { Button } from "../../ui/button";
 
 export const Footer = ({
 	isVertical: isLargeScreen,
 }: { isVertical: boolean }) => {
 	return isLargeScreen ? (
-		<div className="flex flex-col h-full px-3 py-1 bg-white">
+		<div className="flex flex-col h-full px-3 py-1 text-gray-500 bg-gray-50">
 			<div className="flex h-1/4 justify-center items-center">
-				<House className="" />
+				<Button asChild variant="ghost" size="icon">
+					<House className="p-1" />
+				</Button>
 			</div>
 			<div className="flex h-1/4 justify-center items-center">
-				<House className="" />
+				<Button asChild variant="ghost" size="icon">
+					<Search className="p-1" />
+				</Button>
 			</div>
 			<div className="flex h-1/4 justify-center items-center">
-				<House className="" />
+				<Button asChild variant="ghost" size="icon">
+					<Heart className="p-1" />
+				</Button>
 			</div>
 			<div className="flex h-1/4 justify-center items-center">
-				<House className="" />
+				<Button asChild variant="ghost" size="icon">
+					<User className="p-1" />
+				</Button>
 			</div>
 		</div>
 	) : (
-		<div className="flex w-full px-1 py-3 bg-white">
+		<div className="flex w-full px-1 py-3 text-gray-500 bg-white">
 			<div className="flex w-1/4 justify-center items-center">
-				<House className="" />
+				<Button asChild variant="ghost" size="icon">
+					<House className="p-1" />
+				</Button>
 			</div>
 			<div className="flex w-1/4 justify-center items-center">
-				<House className="" />
+				<Button asChild variant="ghost" size="icon">
+					<Search className="p-1" />
+				</Button>
 			</div>
 			<div className="flex w-1/4 justify-center items-center">
-				<House className="" />
+				<Button asChild variant="ghost" size="icon">
+					<Heart className="p-1" />
+				</Button>
 			</div>
 			<div className="flex w-1/4 justify-center items-center">
-				<House className="" />
+				<Button asChild variant="ghost" size="icon">
+					<User className="p-1" />
+				</Button>
 			</div>
 		</div>
 	);
