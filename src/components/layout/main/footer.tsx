a5b8152377e3c759e609b7b832dd265bd5458f12
doguto <doguto110@gmail.com>
2025-05-31 21:08:57 +0900

    Feat: 仮のFooterコンポーネントを作成

--- src/components/layout/main/footer.tsx
+++ src/components/layout/main/footer.tsx
@@ -1,3 +1,35 @@
-export const Footer = () => {
-	return <div>Footer</div>;
+import { House } from "lucide-react";
+
+export const Footer = ({ isLargeScreen }: { isLargeScreen: boolean }) => {
+	return isLargeScreen ? (
+		<div className="flex flex-col h-full px-3 py-1 bg-white">
+			<div className="flex h-1/4 justify-center items-center">
+				<House className="" />
+			</div>
+			<div className="flex h-1/4 justify-center items-center">
+				<House className="" />
+			</div>
+			<div className="flex h-1/4 justify-center items-center">
+				<House className="" />
+			</div>
+			<div className="flex h-1/4 justify-center items-center">
+				<House className="" />
+			</div>
+		</div>
+	) : (
+		<div className="flex w-full px-1 py-3 bg-white">
+			<div className="flex w-1/4 justify-center items-center">
+				<House className="" />
+			</div>
+			<div className="flex w-1/4 justify-center items-center">
+				<House className="" />
+			</div>
+			<div className="flex w-1/4 justify-center items-center">
+				<House className="" />
+			</div>
+			<div className="flex w-1/4 justify-center items-center">
+				<House className="" />
+			</div>
+		</div>
+	);
 };
