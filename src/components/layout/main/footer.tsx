1b7188977f21f3724c927f103c1afa44a71196c9
doguto <doguto110@gmail.com>
2025-06-01 12:17:09 +0900

    Refactor: Footerをmapを使用した形に

--- src/components/layout/main/footer.tsx
+++ src/components/layout/main/footer.tsx
@@ -4,51 +4,32 @@ import { Button } from "../../ui/button";
 export const Footer = ({
 	isVertical: isLargeScreen,
 }: { isVertical: boolean }) => {
+	const icons = [
+		{ component: House, key: "House" },
+		{ component: Search, key: "Search" },
+		{ component: Heart, key: "Heart" },
+		{ component: User, key: "User" },
+	];
+
 	return isLargeScreen ? (
 		<div className="flex flex-col h-full px-3 py-1 text-gray-500 bg-gray-50">
-			<div className="flex h-1/4 justify-center items-center">
-				<Button asChild variant="ghost" size="icon">
-					<House className="p-1" />
-				</Button>
-			</div>
-			<div className="flex h-1/4 justify-center items-center">
-				<Button asChild variant="ghost" size="icon">
-					<Search className="p-1" />
-				</Button>
-			</div>
-			<div className="flex h-1/4 justify-center items-center">
-				<Button asChild variant="ghost" size="icon">
-					<Heart className="p-1" />
-				</Button>
-			</div>
-			<div className="flex h-1/4 justify-center items-center">
-				<Button asChild variant="ghost" size="icon">
-					<User className="p-1" />
-				</Button>
-			</div>
+			{icons.map(({ component: Icon, key }) => (
+				<div key={key} className="flex h-1/4 justify-center items-center">
+					<Button asChild variant="ghost" size="icon">
+						<Icon className="p-1" />
+					</Button>
+				</div>
+			))}
 		</div>
 	) : (
 		<div className="flex w-full px-1 py-3 text-gray-500 bg-white">
-			<div className="flex w-1/4 justify-center items-center">
-				<Button asChild variant="ghost" size="icon">
-					<House className="p-1" />
-				</Button>
-			</div>
-			<div className="flex w-1/4 justify-center items-center">
-				<Button asChild variant="ghost" size="icon">
-					<Search className="p-1" />
-				</Button>
-			</div>
-			<div className="flex w-1/4 justify-center items-center">
-				<Button asChild variant="ghost" size="icon">
-					<Heart className="p-1" />
-				</Button>
-			</div>
-			<div className="flex w-1/4 justify-center items-center">
-				<Button asChild variant="ghost" size="icon">
-					<User className="p-1" />
-				</Button>
-			</div>
+			{icons.map(({ component: Icon, key }) => (
+				<div key={key} className="flex w-1/4 justify-center items-center">
+					<Button asChild variant="ghost" size="icon">
+						<Icon className="p-1" />
+					</Button>
+				</div>
+			))}
 		</div>
 	);
 };
