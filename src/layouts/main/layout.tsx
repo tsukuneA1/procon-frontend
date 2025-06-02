a9279f2173e71e119a4538028e6d4583636f6900
doguto <doguto110@gmail.com>
2025-06-02 18:58:02 +0900

    Fix: tailWindの間違えを修正

--- src/layouts/main/layout.tsx
+++ src/layouts/main/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/sonner";
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="bg-gray-50 flex flex-col">
-			<div className="md:fixed flex justify-center md:justify-start w-full md:z-10 md;top-0">
+			<div className="md:fixed flex justify-center md:justify-start w-full md:z-10 md:top-0">
 				<Header />
 			</div>
 			<div className="flex flex-col md:flex-row">
