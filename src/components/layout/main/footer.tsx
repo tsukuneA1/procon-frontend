d880a7932f1ad138366a484c37faa9b853a045a9
doguto <doguto110@gmail.com>
2025-06-02 18:50:18 +0900

    Fix: より保守性の高い形へ修正

--- src/components/layout/main/footer.tsx
+++ src/components/layout/main/footer.tsx
@@ -4,29 +4,29 @@ import { Button } from "../../ui/button";
 export const Footer = () => {
 	const icons = [
 		{
-			component: <House className="p-1" />,
+			component: <House />,
 			key: "House",
 			label: "トップページへ移動",
 		},
 		{
-			component: <Search className="p-1" />,
+			component: <Search />,
 			key: "Search",
 			label: "検索ページへ移動",
 		},
 		{
-			component: <Heart className="p-1" />,
+			component: <Heart />,
 			key: "Heart",
 			label: "通知ページへ移動",
 		},
 		{
-			component: <User className="p-1" />,
+			component: <User />,
 			key: "User",
 			label: "マイページへ移動",
 		},
 	];
 
 	return (
-		<div className="flex md:flex-col md:h-full md:w-fit w-full px-1 md:px-3 py-3 md:py-1 text-gray-500 bg-gray-50">
+		<div className="flex md:flex-col md:h-full md:py-44 md:w-fit w-full px-1 md:px-3 py-3 text-gray-500 bg-white md:bg-gray-50">
 			{icons.map((icon) => (
 				<div
 					key={icon.key}
@@ -36,7 +36,7 @@ export const Footer = () => {
 						asChild
 						variant="ghost"
 						size="icon"
-						className="cursor-pointer"
+						className="cursor-pointer p-1"
 						aria-label={icon.label}
 					>
 						{icon.component}
--- src/layouts/main/layout.tsx
+++ src/layouts/main/layout.tsx
@@ -4,8 +4,8 @@ import { Toaster } from "@/components/ui/sonner";
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="bg-gray-50">
-			<div className="fixed flex justify-center md:justify-start w-full">
+		<div className="bg-gray-50 flex flex-col">
+			<div className="md:fixed flex justify-center md:justify-start w-full md:z-10 md;top-0">
 				<Header />
 			</div>
 			<div className="flex flex-col md:flex-row">
@@ -13,7 +13,7 @@ export const MainLayout = ({ children }: { children: React.ReactNode }) => {
 					{children}
 				</main>
 				<Toaster />
-				<div className="md:h-full md:w-fit w-full bottom-0 md:py-44 fixed border-t border-border">
+				<div className="md:h-full md:w-fit w-full bottom-0 fixed border-t border-border">
 					<Footer />
 				</div>
 			</div>
