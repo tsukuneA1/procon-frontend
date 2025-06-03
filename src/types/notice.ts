0c89bcbadc8b3e60f8bd8dad4f6c6da3990ec614
doguto <doguto110@gmail.com>
2025-06-03 11:42:16 +0900

    Feat: 通知用の型を定義

---
+++ src/types/notice.ts
@@ -0,0 +1,23 @@
+export type Notice = {
+	id: number;
+	userId: number;
+	user: {
+		id: number;
+		name: string;
+		image: string;
+		isFollowing: boolean;
+	};
+	postId: number;
+	postContent: string;
+	notifiableType: NotifiableType;
+	notifiableId: number;
+	createdAt: string;
+	updatedAt: string;
+};
+
+export enum NotifiableType {
+	Like = "Like",
+	Repost = "Repost",
+	Reply = "Reply",
+	Follow = "Follow",
+}
