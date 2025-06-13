8af55bf6cc472f5817ed6ca791527c8d41aec1e5
tsukuneA1 <tkschr1125@gmail.com>
2025-06-13 21:25:54 +0900

    fix:fixed suspence error

--- src/app/compose/post/page.tsx
+++ src/app/compose/post/page.tsx
@@ -1,13 +1,18 @@
 import { PostCreationDialog } from "@/components/general/postCreationDialog/postCreationDialog";
+import { Suspense } from "react";
 
-const CreatePostPage = () => {
+export default function CreatePostPage() {
 	return (
 		<div className="flex justify-center items-start h-screen pt-10 bg-gray-100 dark:bg-gray-900">
-			{/* PostCreationDialogを呼び出すが、モーダルではなくページの一部として表示 */}
 			<div className="w-full max-w-2xl bg-white dark:bg-black rounded-lg shadow-xl">
-				<PostCreationDialog />
+				<Suspense fallback={<PageLoading />}>
+					<PostCreationDialog />
+				</Suspense>
 			</div>
 		</div>
 	);
+}
+
+const PageLoading = () => {
+	return <div className="p-8">コンテンツを読み込んでいます...</div>;
 };
-export default CreatePostPage;
