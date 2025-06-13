100b19cafece2f96c5938cd10b9ee9cdbc0146be
tsukuneA1 <tkschr1125@gmail.com>
2025-06-13 21:20:50 +0900

    fix:fixed suspence error

--- src/app/@modal/(.)compose/post/page.tsx
+++ src/app/@modal/(.)compose/post/page.tsx
@@ -1,7 +1,12 @@
 import { PostCreationDialog } from "@/components/general/postCreationDialog/postCreationDialog";
+import { Suspense } from "react";
 
 const CreatePostModal = () => {
-	return <PostCreationDialog />;
+	return (
+		<Suspense fallback={<div>loading</div>}>
+			<PostCreationDialog />
+		</Suspense>
+	);
 };
 
 export default CreatePostModal;
